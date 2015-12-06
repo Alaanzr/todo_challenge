33af0aae621b31c2986d279260c285581c05d859
alaanzr <alaanzr@gmail.com>
2015-12-06 01:18:27 +0000

    Edits tasks

--- js/taskController.js
+++ js/taskController.js
@@ -4,12 +4,16 @@ toDoApp.controller('TaskController', function() {
   self.todos = [];
 
   self.addToDo = function() {
-    self.todos.push({content: self.toDoContent, complete: false});
+    self.todos.push({content: self.toDoContent, complete: false, modifyInProgress: false});
     self.toDoContent = '';
   };
 
   self.delete = function(todo) {
     var index = self.todos.indexOf(todo);
     if (index > -1) { self.todos.splice(index, 1); }
+  };
+
+  self.edit = function(todo) {
+    return todo.modifyInProgress ? todo.modifyInProgress = false : todo.modifyInProgress = true;
   };
 });
